dad2802cab95019929626d9e4e2877a95f5dcd53
Bernard Adanlessossi <bernard.adanlessossi@bdch.local>
2019-04-25 15:34:36 +0200

    Added Internationalization and XML Content Negociation

--- src/main/java/com/businessdecision/rest/SpringRestApplication.java
+++ src/main/java/com/businessdecision/rest/SpringRestApplication.java
@@ -1,13 +1,26 @@
 package com.businessdecision.rest;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class SpringRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		
+		localeResolver.setDefaultLocale(Locale.GERMAN);
+		return localeResolver;
 	}
 
 }
--- src/main/java/com/businessdecision/rest/controller/HelloWorldController.java
+++ src/main/java/com/businessdecision/rest/controller/HelloWorldController.java
@@ -3,6 +3,9 @@
  */
 package com.businessdecision.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,9 @@ import com.businessdecision.rest.domain.HelloWorldBean;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(path = "/hello-world")
 	public String sayHello() {
 		return "Hello World of Pain!";
@@ -29,5 +35,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean sayHelloPath(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world/internationalized")
+	public String sayHelloInternationalized() {
+		return messageSource.getMessage("good.morning", null, LocaleContextHolder.getLocale());
 	}
 }
