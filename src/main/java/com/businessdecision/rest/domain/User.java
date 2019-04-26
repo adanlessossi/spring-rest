c52bde3810eb1db95d458f230202c65dbcf4e634
Bernard Adanlessossi <adanlessossi@gmail.com>
2019-04-26 09:54:39 +0200

    Enhanced Swagger Documentation

--- src/main/java/com/businessdecision/rest/domain/User.java
+++ src/main/java/com/businessdecision/rest/domain/User.java
@@ -8,18 +8,24 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @author bernard.adanlessossi
  *
  */
+@ApiModel(description = "Details about Users")
 public class User{
 
 	private Integer userId;
 	
 	@Size(min = 2, message = "Name should have at least 2 characters")
+	@ApiModelProperty(notes = "The Name property should have at least 2 characters")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes = "The Birthdate property should be in the past")
 	private Date birthDate;
 
 	/**
